a6e5796f6f68f763048fa70d81cf597fbaff4f1d
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-03-17 22:07:35 -0700

    Make LibraryActivity 100% unit test covered

--- library_android/src/test/java/org/neotech/library/android/LibraryActivityTest.java
+++ library_android/src/test/java/org/neotech/library/android/LibraryActivityTest.java
@@ -4,6 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import androidx.test.core.app.ActivityScenario;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,11 +17,27 @@ public class LibraryActivityTest {
   @Test
   public void shouldHideButtonAfterClick() {
     try (ActivityScenario<LibraryActivity> scenario = ActivityScenario.launch(LibraryActivity.class)) {
-      scenario.onActivity(this::verifyButtonClicked);
+      scenario.onActivity(this::verifyHideButton);
     }
   }
 
-  private void verifyButtonClicked(LibraryActivity activity) {
+
+  @Test
+  public void shouldUpdateTextWhenButtonClicked() {
+    try (ActivityScenario<LibraryActivity> scenario = ActivityScenario.launch(LibraryActivity.class)) {
+      scenario.onActivity(this::verifyClickButton);
+    }
+  }
+
+  private void verifyClickButton(LibraryActivity activity) {
+    assertThat(((TextView)activity.findViewById(R.id.text)).getText()).isEqualTo("Hello");
+
+    Button button = activity.findViewById(R.id.button);
+    button.performClick();
+    assertThat(((TextView)activity.findViewById(R.id.text)).getText()).isEqualTo("Hello Library!");
+  }
+
+  private void verifyHideButton(LibraryActivity activity) {
     Button button = activity.findViewById(R.id.hide);
     button.performClick();
     assertThat(button.getVisibility()).isEqualTo(View.GONE);
