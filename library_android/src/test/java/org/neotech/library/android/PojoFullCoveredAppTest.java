44e80ee3a08901d07911bc5007b4b717c6b0ee5a
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-03-17 22:02:43 -0700

    Move both unit test to library

--- library_android/src/test/java/org/neotech/library/android/PojoFullCoveredAppTest.java
+++ library_android/src/test/java/org/neotech/library/android/PojoFullCoveredAppTest.java
@@ -1,16 +1,17 @@
-package org.neotech.app;
+package org.neotech.library.android;
 
 import com.google.common.truth.Truth;
 import org.junit.Test;
 import org.neotech.library.android.PojoFullCovered;
 
-public class PojoFullCoveredTest {
+public class PojoFullCoveredAppTest {
 
   @Test
   public void setThing() {
     PojoFullCovered instance = new PojoFullCovered();
+
     instance.setThing("123");
-    Truth.assertThat(instance.getThing()).isEqualTo("123");
+    Truth.assertThat(instance.expectedToBeCoveredInApp()).isEqualTo("123");
   }
 
 }
--- library_android/src/test/java/org/neotech/library/android/PojoFullCoveredTest.java
+++ library_android/src/test/java/org/neotech/library/android/PojoFullCoveredTest.java
@@ -1,17 +1,16 @@
-package org.neotech.app;
+package org.neotech.library.android;
 
 import com.google.common.truth.Truth;
 import org.junit.Test;
 import org.neotech.library.android.PojoFullCovered;
 
-public class PojoFullCoveredAppTest {
+public class PojoFullCoveredTest {
 
   @Test
   public void setThing() {
     PojoFullCovered instance = new PojoFullCovered();
-
     instance.setThing("123");
-    Truth.assertThat(instance.expectedToBeCoveredInApp()).isEqualTo("123");
+    Truth.assertThat(instance.getThing()).isEqualTo("123");
   }
 
 }
