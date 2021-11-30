388bdb8fd410feed5243be5246c8c1e081e42eed
TonyTandAndroid <tonythompsoncmu@gmail.com>
2021-11-30 02:57:29 -0800

    Bump up gradle to 7.3 and android studio gradle tools to 7.0.3

--- app/src/androidTest/java/org/neotech/app/MainActivityTest.java
+++ app/src/androidTest/java/org/neotech/app/MainActivityTest.java
@@ -6,8 +6,8 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
 public class MainActivityTest {
 
   @Rule
-  public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(MainActivity.class);
+  public ActivityScenarioRule<MainActivity> rule = new ActivityScenarioRule<>(MainActivity.class);
 
   @Test
   public void shouldUpdateTextAfterButtonClick() {
